c067765b52d289802691a3a0b545fbc5bb6a2eb7
SAWAIZ2003 <121655456+SAWAIZ2003@users.noreply.github.com>
2024-06-12 18:38:43 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+// alert("hello")  we are using node js not browser
+
+console.log(3+3)
+console.log("hi")
+
+let name = "sawaiz"
+let age = 18
+let  isLoggedIn = false;
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean
+//null => standalone
+//symbol => unique
+
+//object
+
+console.log(typeof "sawaiz")
