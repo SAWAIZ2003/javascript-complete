f0b21e9e868f6f229a78fc2eda9658aebcfba9f2
SAWAIZ2003 <121655456+SAWAIZ2003@users.noreply.github.com>
2024-06-12 19:11:50 +0000

    conversionofdatypes

---
+++ basics/conversionoperation.js
@@ -0,0 +1,26 @@
+let age = null
+let flag = true
+console.log(typeof flag)
+
+// console.log(typeof (age))
+// // console.log(typeof age)
+// //result output is in lowercse
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//notes
+//"33" => 33
+//"33abc" => NaN
+//true =>1; false =>0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 ==> true; 0 => false
+//"" ==> false
+// "hitesh" => true
+let num = 67
+let stringnum = String(num)
+console.log(stringnum)
+console.log(typeof stringnum)
