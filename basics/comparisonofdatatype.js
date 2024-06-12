91b026ca2cb1b36e8676659e11c15d69a7a19407
SAWAIZ2003 <121655456+SAWAIZ2003@users.noreply.github.com>
2024-06-12 19:43:13 +0000

    camaprisonpart2

---
+++ basics/comparisonofdatatype.js
@@ -0,0 +1,18 @@
+let a = 33
+let b = -a
+console.log(b)
+/**operation */
+let str1 = "hi"
+let str2 = "sawaiz"
+let str3 = str1+str2
+console.log(str3)
+console.log("1"+2);
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"@")
+console.log(1+2+"4")
+console.log(+true)
+//increment
+let game = 100
+game++;
+console.log(game)
