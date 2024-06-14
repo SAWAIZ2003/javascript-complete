dd08e98b3046b4bd8b1a9c6cfdcfa2bc85290768
SAWAIZ2003 <121655456+SAWAIZ2003@users.noreply.github.com>
2024-06-14 20:20:00 +0000

    datetime

---
+++ basics/dateandtime.js
@@ -0,0 +1,14 @@
+//dates
+
+// let myDate = new Date()
+// console.log(myDate.toTimeString())
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate)
+
+let myCreateDate = new Date(2023,0,23)
+let my = new Date("01-14-2024")
+console.log(myCreateDate.toDateString())
+console.log(myCreateDate.toLocaleString())
+console.log(my.toLocaleString())
--- basics/numberandmaths.js
+++ basics/numberandmaths.js
@@ -5,3 +5,29 @@ console.log(balance);
 
 console.log(balance.toString().length)
 console.log(balance.toFixed(2)) //decimal 
+
+const other = 23.2455
+console.log(other.toPrecision(2))
+
+const kroos = 10000000
+console.log(kroos.toLocaleString()) //american system
+
+
+/*******Maths******** */
+
+console.log(Math.PI)
+console.log(Math.abs(-7))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2)) // higher than 4
+console.log(Math.floor(4.9)) //gives 4
+console.log(Math.min(4,3,6,8))
+console.log(Math.max(4,5,678,9))
+
+console.log(Math.random());
+// genrates randow values between 0-9
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.ceil(Math.random()*(max-min+1))+min)
