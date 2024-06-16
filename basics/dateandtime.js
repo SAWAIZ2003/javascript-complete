df4aa75f4ef102a5e3bf44e3fa5fac38757f0d39
SAWAIZ2003 <121655456+SAWAIZ2003@users.noreply.github.com>
2024-06-16 10:37:07 +0000

    date and time

--- basics/dateandtime.js
+++ basics/dateandtime.js
@@ -1,14 +1,29 @@
 //dates
 
-// let myDate = new Date()
-// console.log(myDate.toTimeString())
-// console.log(myDate.toString())
-// console.log(myDate.toDateString())
-// console.log(myDate.toLocaleDateString())
-// console.log(typeof myDate)
+let myDate = new Date()
+console.log(myDate.toTimeString())
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(typeof myDate)
 
 let myCreateDate = new Date(2023,0,23)
 let my = new Date("01-14-2024")
 console.log(myCreateDate.toDateString())
 console.log(myCreateDate.toLocaleString())
 console.log(my.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp)
+console.log(my.getTime())
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate.getDay())
+console.log(newDate.getMonth()+1)
+
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
