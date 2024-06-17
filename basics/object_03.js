e29048d0ae4a95622a2c766a1a755ba89ef4d61b
SAWAIZ2003 <121655456+SAWAIZ2003@users.noreply.github.com>
2024-06-17 08:21:54 +0000

    object3

---
+++ basics/object_03.js
@@ -0,0 +1,18 @@
+//destructuring of objects
+const course = {
+     coursename : "javascript",
+     price : "9999",
+     instructor : "theo",
+}
+
+const {instructor} = course
+console.log(instructor)
+
+   //destructing objects in react
+
+// const navbar = ({company}) => {
+     
+// }
+
+// navbar(company = "apple")
+
