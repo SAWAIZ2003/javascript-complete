3e64a8a3dad55c4be61cdd6c54bf4503bf79515b
SAWAIZ2003 <121655456+SAWAIZ2003@users.noreply.github.com>
2024-06-20 20:29:42 +0000

    control flow

---
+++ Control_Flow/intro.js
@@ -0,0 +1,33 @@
+// if statement
+
+// if (2 == '2'){
+//   console.log("less than 50")
+// }
+
+// console.log("temperature is greater than 50")
+
+// const score = 200
+
+// if (score>200){
+//    const b = "damn"
+//    console.log(`power: ${b}`)
+// }
+
+const balance = 1000
+// if(balance>500)
+//   console.log("gg")
+
+if(balance<500){
+  console.log("less than 500")
+}
+
+else if (balance<750){
+  console.log("less than 750")
+}
+
+else if (balance < 900){
+   console.log("less than 900")
+}
+else {
+   console.log("less than 1200")
+}
